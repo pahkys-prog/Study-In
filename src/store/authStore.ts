55f1b0548f41cd9fc597c72eb0549ff23f01cc15
swlog <seowonc123@gmail.com>
2026-02-26 15:23:24 +0900

    feat: Zustand 기반 인증 상태 스토어(authStore) 구현

---
+++ src/store/authStore.ts
@@ -0,0 +1,10 @@
+import { create } from 'zustand';
+
+interface AuthState {
+  isLoggedIn: boolean;
+  // 추후 로그인 API 연동 시 확장 예정 (user 정보, setTokens 등)
+}
+
+export const useAuthStore = create<AuthState>(() => ({
+  isLoggedIn: false,
+}));
